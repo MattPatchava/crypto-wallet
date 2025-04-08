e44fb27f0c7f97891cdcafb735265babeeebb889
Matt Patchava <mattpatchava@outlook.com>
2025-04-08 16:48:11 +1000

    chore: set up @ alias for src path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from '@/pages/public/Home';
+
 function App(): JSX.Element {
     return (
-        <div>
-        </div>
+        <Routes>
+            <Route index element={<Home />} />
+        </Routes>
     );
 }
 
