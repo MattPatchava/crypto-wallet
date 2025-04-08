ec755921c4382ee1ecffef13eaa290651fd6f5b4
Matt Patchava <mattpatchava@outlook.com>
2025-04-08 19:46:26 +1000

    feat: fetch and display ETH price on home page

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -1,9 +1,26 @@
 import Header from '@/components/Header';
+import { useState, useEffect } from 'react';
+import { getPrice } from '@/lib/blockchain/chainlink/getPrices';
 
 function Home(): JSX.Element {
+
+    const [ethPrice, setEthPrice] = useState();
+
+    useEffect(() => {
+        async function fetchEthPrice() {
+            const result = await getPrice("eth");
+            setEthPrice(result);
+        }
+
+        fetchEthPrice();
+    }, []);
+
     return (
         <div>
             <Header />
+            <div className="m-4">
+                {ethPrice ? `$${ethPrice}` : "Loading..."}
+            </div>
         </div>
     );
 }
