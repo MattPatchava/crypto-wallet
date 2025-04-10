d2f18571dd33124bf04aab5b8d35b13ed8ff03ad
Matt Patchava <mattpatchava@outlook.com>
2025-04-11 00:38:11 +1000

    style: add vertical padding to heading in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Flex, Heading } from '@chakra-ui/react';
 function Header(): JSX.Element {
     return (
         <Flex px={4} py={2} shadow="md" align="center" justify="space-between">
-            <Heading>Crypto Wallet</Heading>
+            <Heading py={2}>Crypto Wallet</Heading>
         </Flex>
     );
 }
